8540b96048ebc4bcfe2d6678db77367e1b51fab9
Oscar Barlow <oscar.barlow@gmail.com>
2016-10-21 18:08:42 +0100

    Refactor DeckModule and DeckClass to be a bit swankier

--- DeckClass.rb
+++ DeckClass.rb
@@ -7,7 +7,7 @@ require "./DeckModule.rb"
 class ShuffledDeck
 
   def initialize
-    @order = shuffled_deck
+    @order = DeckModule.shuffled_deck
   end
 
   def order # mainly for debugging. So I can get/look inside a ShuffledDeck object
