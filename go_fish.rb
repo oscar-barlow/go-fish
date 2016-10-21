126ad11d348579faa6ca53a158597ec9b4bb7271
Oscar Barlow <oscar.barlow@gmail.com>
2016-10-21 19:19:25 +0100

    wtf? oh well seems to work now

--- go_fish.rb
+++ go_fish.rb
@@ -2,18 +2,33 @@ require "./DeckClass.rb"
 
 GoFishDeck = ShuffledDeck.new
 
+def prompt
+  print "> "
+end
+
 def make_hand(i)
   hand = Array.new
   i.times do
-    hand.push(GoFishDeck.sample)
+    card = GoFishDeck.sample
+    hand.push(card)
+    GoFishDeck.delete_card(card)
   end
   hand
 end
 
-def show_hand
+def show_hand(array)
   line_width = 80
-  h = make_hand(i)
-
   puts "- Your Hand -".center line_width
-  puts h.join(" / ").center line_width
+  puts array.join(" / ").center line_width
+  print "\n"
+end
+
+while true
+  puts "How many cards shall I deal you?"
+  prompt
+  dealout = gets.chomp.to_i
+  player_hand = make_hand(dealout)
+  show_hand(player_hand)
+  puts "Let's chuck those away."
+  puts "There are #{GoFishDeck.order.length} cards left."
 end
