165e32298b6d98edf5a2f8fe2586597e3d8261a3
Oscar Barlow <oscar.barlow@gmail.com>
2016-11-20 19:13:49 +0000

    use helper method to create test objects in card spec

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -2,14 +2,20 @@ require 'card'
 
 describe Card do
 
-  subject(:card) {described_class.new(3, "♤")}
-
   it 'has a value that is an integer' do
-    expect(card.value).to be_kind_of Integer
+    card = create_card(value: 3)
+    expect(card.value).to be 3
   end
 
   it 'has a suit' do
+    card = create_card(suit: "♤")
     expect(card.suit).to match /♤|♧|♡|♢/
   end
 
+end
+
+def create_card(arguments)
+  value = arguments[:value] ||= 7
+  suit =  arguments[:suit] ||= "♤"
+  Card.new(value, suit)
 end
