56303c0404b3898eb4b4cd5933b0448fe50529d9
Oscar Barlow <oscar.barlow@gmail.com>
2016-11-20 19:48:00 +0000

    reorganise card spec

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -2,59 +2,56 @@ require 'card'
 
 describe Card do
 
+  describe '#value' do
+
   it 'has a value that is an integer' do
     card = create_card(value: 3)
     expect(card.value).to be 3
   end
+
+end
+
+describe '#suit' do
 
   it 'has a suit' do
     card = create_card(suit: "♤")
     expect(card.suit).to match /♤|♧|♡|♢/
   end
 
-  it 'checks if the card value is >10' do
-    card = create_card(value: 11)
-    expect(card.greater_than_ten?).to be_truthy
-  end
+end
 
-  it 'returns Jack for a card value of 11' do
-    card = create_card(value: 11)
-    expect(card.rank_name).to eq "Jack"
-  end
+  describe '#rank' do
 
-  it 'stores rank in an instance variable' do
-    card = create_card(value: 11)
-    expect(card.rank).to eq "Jack"
-  end
+    it 'doesn\'t store a rank for cards with a value of less than 10' do
+      card = create_card(value: 9)
+      expect(card.rank).to be_nil
+    end
 
-  it 'doesn\'t store a rank for cards with a value of less than 10' do
-    card = create_card(value: 9)
-    expect(card.rank).to be_nil
-  end
+    it "stores something in rank for Aces" do
+      card = create_card(value: 1)
+      expect(card.rank).to be_truthy
+    end
 
-  it 'knows when a card is an Ace' do
-    card = create_card(value: 1)
-    expect(card.is_ace?).to be_truthy
-  end
+    it 'stores a rank of "Ace" for Aces' do
+      card = create_card(value: 1)
+      expect(card.rank).to eq "Ace"
+    end
 
-  it "stores something in rank for Aces" do
-    card = create_card(value: 1)
-    expect(card.rank).to be_truthy
-  end
+    it 'stores a rank of "Jack" for cards with value 11' do
+      card = create_card(value: 11)
+      expect(card.rank).to eq "Jack"
+    end
 
-  it 'stores a rank of "Ace" for Aces' do
-    card = create_card(value: 1)
-    expect(card.rank).to eq "Ace"
-  end
+    it 'stores a rank of "Queen" for cards with value 12' do
+      card = create_card(value: 12)
+      expect(card.rank).to eq "Queen"
+    end
 
-  it 'stores a rank of "Queen" for cards with value 12' do
-    card = create_card(value: 12)
-    expect(card.rank).to eq "Queen"
-  end
+    it 'stores a rank of "King" for cards with value 13' do
+      card = create_card(value: 13)
+      expect(card.rank).to eq "King"
+    end
 
-  it 'stores a rank of "King" for cards with value 13' do
-    card = create_card(value: 13)
-    expect(card.rank).to eq "King"
   end
 
 end
